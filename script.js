9680adca477431856cb9aec1570e7fc419a72c7d
Samuel Felipe <samuelfelipef@gmail.com>
2021-07-28 16:05:47 -0300

    distribuicao aleatoria de cartas fucionando para todos os casos

--- script.js
+++ script.js
@@ -55,15 +55,13 @@ const baralho = [
     name: "unicornparrot",
     image: "assets/unicornparrot.gif"
   }
-]
+];
+
+let cards = [];
+let cardsSort = [];
 let escolhidas = [];
 
-
-function carregarTela() {
-
-  baralho.sort(() => {
-    return Math.random() - 0.5;
-  })
+function loadGame() {
 
   let numCartas = Number(prompt("Digite com quantas quartas vc quer jogar. (4 a 14, valores pares)."));
 
@@ -73,25 +71,53 @@ function carregarTela() {
   
   let tabuleiro = document.querySelector(".game");
   let cont = 0;
+  let cont2 = 0;
   while(cont != numCartas) {
     if(cont % 2 == 0) {
-      tabuleiro.innerHTML += `
-      <div class="card">
-        <img class="front-card" id="${cont}" name="${baralho[cont].name}" src="/assets/front.png" alt="frontcard" onclick="escolherCarta(this)">
-      </div>`
+      cards.push(baralho[cont]);
     } else {
-      tabuleiro.innerHTML += `
-      <div class="card">
-        <img class="front-card" id="${cont}" name="${baralho[cont].name}" src="/assets/front.png" alt="frontcard" onclick="escolherCarta(this)">
-      </div>`
+      cards.push(baralho[cont]);
     }
     cont++;
   }
+
+  cardsSort = cards.sort(() => {
+    return Math.random() - 0.5;
+  });
+
+  while (cont2 < cards.length) {
+    tabuleiro.innerHTML += `
+    <div class="card">
+    <img class="front-card" id="${cont2}" name="${cardsSort[cont2].name}" src="/assets/front.png" alt="frontcard" onclick="pickCard(this)">
+   </div>`
+   cont2++;
+  }
 }
 
-function escolherCarta(carta) {
+function pickCard(carta) {
   
-  carta.src = baralho[carta.id].image;
+  carta.src = cardsSort[carta.id].image;
+  escolhidas.push(carta);
+
+  if(escolhidas.length === 2) {
+    setTimeout(() => {
+      let carta1 = escolhidas[0];
+      let carta2 = escolhidas[1];
+      console.log(escolhidas);
+
+      if(carta1.name === carta2.name) {
+        carta1.removeEventListener("click", escolherCarta);
+        carta2.removeEventListener("click", escolherCarta);
+        alert("iguais!!")
+      } else {
+        carta1.src = "/assets/front.png";
+        carta2.src = "/assets/front.png";
+      }
+
+      escolhidas = [];
+    }, 1000)
+  }
+
 }
  
 
